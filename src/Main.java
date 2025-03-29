3705bb0b558cfa3b2109dac9ccb5bdf6a5ed7fc6
Eduardo433781 <eduardo.coerse@gmail.com>
2025-03-29 17:28:08 -0300

    feat: GestãoEscolar

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,8 @@ public class Main {
         Aluno aluno2 = new Aluno("Mariana Souza", 16, "A456");
 
         // Criando professores
-        Professor professor1 = new Professor("João Pereira", "Matemática", 15);
-        Professor professor2 = new Professor("Ana Costa", "Português", 15);
+        Professor professor1 = new Professor("João Pereira", "Matemática", 40);
+        Professor professor2 = new Professor("Ana Costa", "Português", 55);
 
         // Adicionando alunos e professores à escola
         escola.adicionarAluno(aluno1); // Correção no nome do método
@@ -44,5 +44,24 @@ public class Main {
 
         // Exibindo os detalhes da disciplina
         disciplina.exibirDetalhes();
+        System.out.println("-----------------------------------------------------------------------------------------------------------");
+
+
+        // Criando uma turma e associando um professor
+        Turma turma2 = new Turma("Java2", 2025, professor2);
+
+        // Adicionando alunos à turma
+        turma2.adicionarAluno(aluno1);
+        turma2.adicionarAluno(aluno2);
+
+        // Exibindo a lista de alunos da turma
+        turma2.listarAlunos();
+        System.out.println("-----------------------------------------------------------------------------------------------------------");
+
+        // Criando uma disciplina e associando um professor
+        Disciplina disciplina2 = new Disciplina("Português", 80, professor2);
+
+        // Exibindo os detalhes da disciplina
+        disciplina2.exibirDetalhes();
     }
 }
